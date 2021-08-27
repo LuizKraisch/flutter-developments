8d1c9484b3d2562dad0216b2b9bcc46363aa40b1
LuizKraisch <luizkraisch22@gmail.com>
2021-08-26 22:16:38 -0300

    Dev Card Finished

--- lib/main.dart
+++ lib/main.dart
@@ -15,37 +15,70 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                color: Colors.white,
-                width: 100.0,
-                height: 100.0,
-                padding: EdgeInsets.all(10.0),
-                child: Text('Container 1')
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: NetworkImage('https://media-exp1.licdn.com/dms/image/C4E03AQE4CFJRQ29LnA/profile-displayphoto-shrink_200_200/0/1621020292662?e=1634169600&v=beta&t=Jf2cHxTgEGTqTsSNabbM89NXTkaMJ7fYUZObd7COesw'),
+              ),
+              Text('Luiz Kraisch',
+                style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Poppins Bold',
+                ),
+              ),
+              Text('SOFTWARE DEVELOPER',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Poppins Light',
+                  letterSpacing: 2.5
+                ),
               ),
               SizedBox(
                 height: 20.0,
+                width: 180.0,
+                child: (
+                  Divider(
+                    color: Colors.white,
+                  )
+                ),
               ),
-              Container(
-                color: Colors.red,
-                width: 100.0,
-                height: 100.0,
-                padding: EdgeInsets.all(10.0),
-                child: Text('Container 2')
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.lightBlue
+                  ),
+                  title: Text(
+                    '(47) 9 92514394',
+                    style: TextStyle(
+                      color: Colors.lightBlue,
+                      fontFamily: 'Poppins Light',
+                      fontSize: 20.0,
+                    )
+                  )
+                ),
               ),
-                SizedBox(
-                height: 20.0,
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: Icon(
+                        Icons.email,
+                        color: Colors.lightBlue
+                    ),
+                    title: Text(
+                        'luizkraisch22@gmail.com',
+                        style: TextStyle(
+                          color: Colors.lightBlue,
+                          fontFamily: 'Poppins Light',
+                          fontSize: 18.0,
+                        )
+                    )
+                ),
               ),
-              Container(
-                  color: Colors.green,
-                  width: 100.0,
-                  height: 100.0,
-                  padding: EdgeInsets.all(10.0),
-                  child: Text('Container 3')
-              ),
-              Container(
-                width: double.infinity,
-                height: 20.0,
-              )
             ],
           ),
         ),
