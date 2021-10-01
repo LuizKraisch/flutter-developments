619a88cc8637fc80697bae14d947893fbf573d23
LuizKraisch <luizkraisch22@gmail.com>
2021-09-30 22:31:09 -0300

    Projeto finalizado

---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class BottomButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final String title;
+
+  BottomButton({required this.onTap, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            title,
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+        color: kButtonColor,
+        height: 80.0,
+      ),
+    );
+  }
+}
